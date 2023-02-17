8ea72dace2f51098abcde9ff55ab80850850fb52
Bright Kunda Mulomba <89812566+BSK-98@users.noreply.github.com>
2023-02-17 23:25:59 +0200

    modified the scripts file

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
   loader (messageDiv);
 
   // fetch data from server
-  const response = await fetch ('http://localhost:5000/', {
+  const response = await fetch ('https://nad-codegpt.onrender.com/', {
     method: "POST",
     headers: {
       'Content-type': 'application/json',
